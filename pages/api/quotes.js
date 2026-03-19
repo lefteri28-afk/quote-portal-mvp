46df0cd176b733380bcbd34b5c5f58747f6d56ce
lefteri28-afk <lefteri28@gmail.com>
2026-03-19 15:40:23 -0700

    Update quotes.js

--- pages/api/quotes.js
+++ pages/api/quotes.js
@@ -1,4 +1,5 @@
-import { formidable } from 'formidable';
+import formidable from 'formidable';
+``
 import fs from 'fs';
 import path from 'path';
 import { parseQuoteXlsx } from '../../lib/xlsx.js';
